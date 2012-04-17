2d74fb9599ed7b95f5266826d757929bd3feb3e1
Francois LAROCHE <flaroche@gmail.com>
2012-04-17 23:22:48 +0200

    javadoc

--- src/main/java/org/ubiquity/Copier.java
+++ src/main/java/org/ubiquity/Copier.java
@@ -3,16 +3,36 @@ package org.ubiquity;
 import java.util.List;
 
 /**
+ * This interface defines a Copier, used to copy an object into another, or map an object into another one of another class.
+ *
  * @author François LAROCHE
  *
- * @param <T>
- * @param <U>
+ * @param <T> the source object class
+ * @param <U> the destination object class
  */
 public interface Copier<T, U> {
 
+    /**
+     * Copies the values of the object given in a new object of the destination class.
+     *
+     * @param element the object to copy into a new destination object
+     * @return a new object of the destination, in which have been copied the properties
+     */
 	U map(T element);
-	
+
+    /**
+     * Copy a list of elements of the source class into a list of new elements of the destination class
+     *
+     * @param elements the source objects
+     * @return objects of the destination class, containing the objects
+     */
 	List<U> map(List<T> elements);
 
+    /**
+     * Copy an object properties into another.
+     *
+     * @param source the source object, from which to read the properties
+     * @param destination the destination object, in which to write the properties
+     */
     void copy(T source, U destination);
 }
--- src/main/java/org/ubiquity/bytecode/SimpleCopier.java
+++ src/main/java/org/ubiquity/bytecode/SimpleCopier.java
@@ -6,16 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Copier stub, making implementations easier.
+ *
+ * @param <T></T> The source class
+ * @param <U></U> the destination class
+ *
  * @author François LAROCHE
  */
 public abstract class SimpleCopier <T, U> implements Copier<T, U>{
 
     protected final CopyContext context;
 
+    /**
+     * Create a new {@link SimpleCopier} setting the {@link CopyContext} to be used for conversions
+     *
+     * @param context the {@link CopyContext} to be used to copy complex objects
+     */
     protected SimpleCopier(CopyContext context) {
         this.context = context;
     }
 
+    /**
+     * @inheritdoc
+     */
     @Override
     public List<U> map(List<T> elements) {
         List<U> result = new ArrayList<U>(elements.size());
@@ -25,6 +38,9 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return result;
     }
 
+    /**
+     * @inheritdoc
+     */
     @Override
     public U map(T element) {
         U target = newInstance();
@@ -32,8 +48,22 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return target;
     }
 
+    /**
+     * Instanciate an object of the U class.
+     * This method is needed, because instanciating U from reflexion is slow.
+     * Implementors are expected to implement it with a "new".
+     *
+     * @return a newly instanciated object of class U
+     */
     protected abstract U newInstance();
 
+    /**
+     * converts a java.lang.Integer to an int.
+     * Of the given parameter is null, this method will return 0.
+     *
+     * @param value the java.lang.Integer to convert
+     * @return the converted int
+     */
     protected int objectToPrimitive(Integer value) {
         if(value != null) {
             return value.intValue();
@@ -41,6 +71,13 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return 0;
     }
 
+    /**
+     * converts a java.lang.Boolean to a boolean.
+     * Of the given parameter is null, this method will return false.
+     *
+     * @param value the java.lang.Boolean to convert
+     * @return the converted boolean
+     */
     protected boolean objectToPrimitive(Boolean value) {
         if(value != null) {
             return value.booleanValue();
@@ -48,6 +85,13 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return false;
     }
 
+    /**
+     * converts a java.lang.Long to a long.
+     * Of the given parameter is null, this method will return 0.
+     *
+     * @param value the java.lang.Long to convert
+     * @return the converted long
+     */
     protected long objectToPrimitive(Long value) {
         if(value != null) {
             return value.longValue();
@@ -55,6 +99,13 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return 0l;
     }
 
+    /**
+     * converts a java.lang.Double to a double.
+     * Of the given parameter is null, this method will return 0.
+     *
+     * @param value the java.lang.Double to convert
+     * @return the converted double
+     */
     protected double objectToPrimitive(Double value) {
         if(value != null) {
             return value.doubleValue();
@@ -62,6 +113,13 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return 0d;
     }
 
+    /**
+     * converts a java.lang.Float to a float.
+     * Of the given parameter is null, this method will return 0.
+     *
+     * @param value the java.lang.Float to convert
+     * @return the converted float
+     */
     protected float objectToPrimitive(Float value) {
         if(value != null) {
             return value.floatValue();
@@ -69,6 +127,13 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return 0f;
     }
 
+    /**
+     * converts a java.lang.Character to a char.
+     * Of the given parameter is null, this method will return the char wielding the 0 value.
+     *
+     * @param value the java.lang.Character to convert
+     * @return the converted char
+     */
     protected char objectToPrimitive(Character value) {
         if(value != null) {
             return value.charValue();
@@ -76,6 +141,13 @@ public abstract class SimpleCopier <T, U> implements Copier<T, U>{
         return (char)0;
     }
 
+    /**
+     * converts a java.lang.Byte to a byte.
+     * Of the given parameter is null, this method will return 0.
+     *
+     * @param value the java.lang.Bte to convert
+     * @return the converted byte
+     */
     protected byte objectToPrimitive(Byte value) {
         if(value != null) {
             return value.byteValue();
