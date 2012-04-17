fc6149f13a75a2204ccdfd9f82b570ccc5483ca6
Francois LAROCHE <flaroche@gmail.com>
2012-04-17 23:53:53 +0200

    Refactor to make generator more readable

--- src/main/java/org/ubiquity/bytecode/CopierGenerator.java
+++ src/main/java/org/ubiquity/bytecode/CopierGenerator.java
@@ -32,6 +32,54 @@ public class CopierGenerator {
 			throw new IllegalStateException("Unable to parse class : ", e);
 		}
 	}
+
+    private static void createConstructor(ClassWriter writer, String className) {
+        MethodVisitor visitor = writer.visitMethod(ACC_PUBLIC, "<init>", "(Lorg/ubiquity/bytecode/CopyContext;)V", null, null);
+        Label label1 = new Label();
+        visitor.visitLabel(label1);
+        visitor.visitVarInsn(ALOAD, 0);
+        visitor.visitVarInsn(ALOAD, 1);
+        visitor.visitMethodInsn(INVOKESPECIAL, "org/ubiquity/bytecode/SimpleCopier", "<init>", "(Lorg/ubiquity/bytecode/CopyContext;)V");
+        Label label2 = new Label();
+        visitor.visitLabel(label2);
+        visitor.visitInsn(RETURN);
+        Label label3 = new Label();
+        visitor.visitLabel(label3);
+        visitor.visitLocalVariable("this", getDescription(className), null, label1, label3, 0);
+        visitor.visitLocalVariable("context", "Lorg/ubiquity/bytecode/CopyContext;", null, label1, label3, 1);
+        visitor.visitMaxs(2, 2);
+        visitor.visitEnd();
+    }
+
+    private static  void createNewInstance(ClassWriter writer, String srcName, String destinationName) {
+        MethodVisitor visitor = writer.visitMethod(ACC_PROTECTED + ACC_VOLATILE + ACC_BRIDGE, "newInstance", "()Ljava/lang/Object;", null, null);
+        visitor.visitVarInsn(ALOAD, 0);
+        visitor.visitMethodInsn(INVOKEVIRTUAL, srcName, "newInstance", "()" + getDescription(destinationName));
+        visitor.visitInsn(ARETURN);
+        visitor.visitMaxs(1, 1);
+        visitor.visitEnd();
+
+        visitor = writer.visitMethod(ACC_PROTECTED, "newInstance", "()" + getDescription(destinationName), null, null);
+        visitor.visitTypeInsn(NEW, destinationName);
+        visitor.visitInsn(DUP);
+        visitor.visitMethodInsn(INVOKESPECIAL, destinationName, "<init>", "()V");
+        visitor.visitInsn(ARETURN);
+        visitor.visitMaxs(2, 1);
+        visitor.visitEnd();
+    }
+
+    private static  void createCopyBridge(ClassWriter writer, String className, String srcName, String destinationName) {
+        MethodVisitor visitor = writer.visitMethod(ACC_PUBLIC + ACC_VOLATILE + ACC_BRIDGE, "copy", "(Ljava/lang/Object;Ljava/lang/Object;)V", null, null);
+        visitor.visitVarInsn(ALOAD, 0);
+        visitor.visitVarInsn(ALOAD, 1);
+        visitor.visitTypeInsn(CHECKCAST, srcName);
+        visitor.visitVarInsn(ALOAD, 2);
+        visitor.visitTypeInsn(CHECKCAST, destinationName);
+        visitor.visitMethodInsn(INVOKEVIRTUAL, className, "copy", '(' + getDescription(srcName) + getDescription(destinationName) + ")V");
+        visitor.visitInsn(RETURN);
+        visitor.visitMaxs(3, 3);
+        visitor.visitEnd();
+    }
 	
 	public static <T, U> Copier<T, U> createCopier(Class<T> src, Class<U> destination, CopyContext ctx)
             throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
@@ -45,23 +93,11 @@ public class CopierGenerator {
                 "Lorg/ubiquity/bytecode/SimpleCopier<" + getDescription(srcName) + getDescription(destinationName) + ">;",
                 "org/ubiquity/bytecode/SimpleCopier", null);
 
-        MethodVisitor visitor = writer.visitMethod(ACC_PUBLIC, "<init>", "(Lorg/ubiquity/bytecode/CopyContext;)V", null, null);
-        Label label1 = new Label();
-        visitor.visitLabel(label1);
-        visitor.visitVarInsn(ALOAD, 0);
-        visitor.visitVarInsn(ALOAD, 1);
-        visitor.visitMethodInsn(INVOKESPECIAL, "org/ubiquity/bytecode/SimpleCopier", "<init>", "(Lorg/ubiquity/bytecode/CopyContext;)V");
-        Label label2 = new Label();
-        visitor.visitLabel(label2);
-        visitor.visitInsn(RETURN);
-        Label label3 = new Label();
-        visitor.visitLabel(label3);
-        visitor.visitLocalVariable("this", getDescription(className), null, label1, label3, 0);
-        visitor.visitLocalVariable("context", getDescription(className), null, label1, label3, 1);
-        visitor.visitMaxs(2, 2);
-        visitor.visitEnd();
+        createConstructor(writer, className);
+        createNewInstance(writer, className, destinationName);
+        createCopyBridge(writer, className, srcName, destinationName);
 
-        visitor = writer.visitMethod(ACC_PUBLIC + ACC_FINAL, "copy", '(' + getDescription(srcName) + getDescription(destinationName) + ")V", null, null);
+        MethodVisitor visitor = writer.visitMethod(ACC_PUBLIC + ACC_FINAL, "copy", '(' + getDescription(srcName) + getDescription(destinationName) + ")V", null, null);
         for(Property p : properties) {
             visitor.visitVarInsn(ALOAD, 2);
             visitor.visitVarInsn(ALOAD, 1);
@@ -71,32 +107,6 @@ public class CopierGenerator {
         visitor.visitVarInsn(ALOAD, 2);
         visitor.visitInsn(RETURN);
         visitor.visitMaxs(3,3);
-        visitor.visitEnd();
-
-        visitor = writer.visitMethod(ACC_PUBLIC + ACC_VOLATILE + ACC_BRIDGE, "copy", "(Ljava/lang/Object;Ljava/lang/Object;)V", null, null);
-        visitor.visitVarInsn(ALOAD, 0);
-        visitor.visitVarInsn(ALOAD, 1);
-        visitor.visitTypeInsn(CHECKCAST, srcName);
-        visitor.visitVarInsn(ALOAD, 2);
-        visitor.visitTypeInsn(CHECKCAST, destinationName);
-        visitor.visitMethodInsn(INVOKEVIRTUAL, className, "copy", '(' + getDescription(srcName) + getDescription(destinationName) + ")V");
-        visitor.visitInsn(RETURN);
-        visitor.visitMaxs(3, 3);
-        visitor.visitEnd();
-
-        visitor = writer.visitMethod(ACC_PROTECTED + ACC_VOLATILE + ACC_BRIDGE, "newInstance", "()Ljava/lang/Object;", null, null);
-        visitor.visitVarInsn(ALOAD, 0);
-        visitor.visitMethodInsn(INVOKEVIRTUAL, className, "newInstance", "()" + getDescription(destinationName));
-        visitor.visitInsn(ARETURN);
-        visitor.visitMaxs(1, 1);
-        visitor.visitEnd();
-
-        visitor = writer.visitMethod(ACC_PROTECTED, "newInstance", "()" + getDescription(destinationName), null, null);
-        visitor.visitTypeInsn(NEW, destinationName);
-        visitor.visitInsn(DUP);
-        visitor.visitMethodInsn(INVOKESPECIAL, destinationName, "<init>", "()V");
-        visitor.visitInsn(ARETURN);
-        visitor.visitMaxs(2, 1);
         visitor.visitEnd();
 
         writer.visitEnd();
