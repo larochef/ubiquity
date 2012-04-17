77f55b18b1c190d05101e777aeec64ce73d84e35
Francois LAROCHE <flaroche@gmail.com>
2012-04-18 00:10:05 +0200

    Compare speed with beanutils.copy

--- src/test/java/org/ubiquity/bytecode/PropertyRetrieverVisitorTest.java
+++ src/test/java/org/ubiquity/bytecode/PropertyRetrieverVisitorTest.java
@@ -50,6 +50,25 @@ public class PropertyRetrieverVisitorTest {
 	}
 
     @Test
+    public void testCopySpeed() throws Exception {
+        SimpleTestClass src = new SimpleTestClass();
+        src.setProperty1("property1");
+        src.setProperty3("property3");
+        long start = System.currentTimeMillis();
+        CopyContext ctx = new CopyContext();
+        Copier<SimpleTestClass, SimpleTestClass> copier = CopierGenerator.createCopier(SimpleTestClass.class, ctx);
+        long start2 = System.currentTimeMillis();
+        copier.copy(src, new SimpleTestClass());
+        long end = System.currentTimeMillis();
+        System.out.println("Asm process took : " + (end - start) + "ms, copy took " + (end - start2) + "ms");
+        start = System.currentTimeMillis();
+        BeanUtils.copyProperties(new SimpleTestClass(), src);
+        end = System.currentTimeMillis();
+        System.out.println("BeanUtils process took : " + (end - start) + "ms");
+
+    }
+
+    @Test
     public void testCopierCreation() throws Exception {
         CopyContext ctx = new CopyContext();
         Copier<SimpleTestClass, SimpleTestClass> copier = CopierGenerator.createCopier(SimpleTestClass.class, ctx);
