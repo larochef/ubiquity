cfb1e8662122a0cebe91396dbac83fb9db4eb8fb
Francois LAROCHE <flaroche@gmail.com>
2012-04-21 22:41:06 +0200

    Simple test for Ubiquity class

---
+++ src/test/java/org/ubiquity/UbiquityTest.java
@@ -0,0 +1,25 @@
+package org.ubiquity;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Date: 21/04/12
+ *
+ * @author François LAROCHE
+ */
+public class UbiquityTest {
+
+    @Test
+    public void testUbiquity() {
+        Ubiquity ubiquity = new Ubiquity();
+        assertEquals(ArrayList.class, ubiquity.getDefaultListImplementation());
+        assertEquals(HashSet.class, ubiquity.getDefaultSetImplementation());
+        assertEquals(HashMap.class, ubiquity.getDefaultMapImplementation());
+    }
+}
