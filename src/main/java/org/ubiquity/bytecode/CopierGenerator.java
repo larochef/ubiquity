dc1361152f7e29b1fb1df148532754d131dd2086
Francois LAROCHE <flaroche@gmail.com>
2012-04-21 11:14:04 +0200

    Add visitVariable

--- src/main/java/org/ubiquity/bytecode/CopierGenerator.java
+++ src/main/java/org/ubiquity/bytecode/CopierGenerator.java
@@ -78,18 +78,28 @@ class CopierGenerator {
     }
 
     private static  void createNewInstance(ClassWriter writer, String className, String destinationName) {
+        Label label1 = new Label();
+        Label label2 = new Label();
         MethodVisitor visitor = writer.visitMethod(ACC_PROTECTED + ACC_VOLATILE + ACC_BRIDGE, "newInstance", "()Ljava/lang/Object;", null, null);
+        visitor.visitLabel(label1);
         visitor.visitVarInsn(ALOAD, 0);
         visitor.visitMethodInsn(INVOKEVIRTUAL, className, "newInstance", "()" + getDescription(destinationName));
         visitor.visitInsn(ARETURN);
+        visitor.visitLabel(label2);
+        visitor.visitLocalVariable("this", getDescription(className), null, label1, label2, 0);
         visitor.visitMaxs(1, 1);
         visitor.visitEnd();
 
+        label1 = new Label();
+        label2 = new Label();
         visitor = writer.visitMethod(ACC_PROTECTED, "newInstance", "()" + getDescription(destinationName), null, null);
+        visitor.visitLabel(label1);
         visitor.visitTypeInsn(NEW, destinationName);
         visitor.visitInsn(DUP);
         visitor.visitMethodInsn(INVOKESPECIAL, destinationName, "<init>", "()V");
         visitor.visitInsn(ARETURN);
+        visitor.visitLabel(label2);
+        visitor.visitLocalVariable("this", getDescription(className), null, label1, label2, 0);
         visitor.visitMaxs(2, 1);
         visitor.visitEnd();
     }
@@ -124,7 +134,10 @@ class CopierGenerator {
     }
 
     private static  void createCopyBridge(ClassWriter writer, String className, String srcName, String destinationName) {
+        Label label1 = new Label();
+        Label label2 = new Label();
         MethodVisitor visitor = writer.visitMethod(ACC_PUBLIC + ACC_VOLATILE + ACC_BRIDGE, "copy", "(Ljava/lang/Object;Ljava/lang/Object;)V", null, null);
+        visitor.visitLabel(label1);
         visitor.visitVarInsn(ALOAD, 0);
         visitor.visitVarInsn(ALOAD, 1);
         visitor.visitTypeInsn(CHECKCAST, srcName);
@@ -132,6 +145,10 @@ class CopierGenerator {
         visitor.visitTypeInsn(CHECKCAST, destinationName);
         visitor.visitMethodInsn(INVOKEVIRTUAL, className, "copy", '(' + getDescription(srcName) + getDescription(destinationName) + ")V");
         visitor.visitInsn(RETURN);
+        visitor.visitLabel(label2);
+        visitor.visitLocalVariable("this", getDescription(className), null, label1, label2, 0);
+        visitor.visitLocalVariable("x0", "Ljava/lang/Object;", null, label1, label2, 1);
+        visitor.visitLocalVariable("x1", "Ljava/lang/Object;", null, label1, label2, 2);
         visitor.visitMaxs(3, 3);
         visitor.visitEnd();
     }
