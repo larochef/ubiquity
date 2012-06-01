a9b08534d6dbcd5aa60597069d5160ea9d156e5c
Francois LAROCHE <flaroche@gmail.com>
2012-06-02 01:43:14 +0200

    Test array boxing, both sides

--- src/test/java/org/ubiquity/bytecode/TestBoxedArrays.java
+++ src/test/java/org/ubiquity/bytecode/TestBoxedArrays.java
@@ -1,6 +1,9 @@
 package org.ubiquity.bytecode;
 
+import org.junit.Test;
 import org.ubiquity.Ubiquity;
+import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Date: 01/06/12
@@ -157,6 +160,149 @@ public class TestBoxedArrays {
 
         public void setShorts(Short[] shorts) {
             this.shorts = shorts;
+        }
+    }
+
+    @Test
+    public void testNullArrays() {
+        PrimitiveArrayClass primitive = new PrimitiveArrayClass();
+        BoxedArrayClass boxed = ubiquity.map(primitive, BoxedArrayClass.class);
+
+        assertNull(boxed.getBools());
+        assertNull(boxed.getBytes());
+        assertNull(boxed.getChars());
+        assertNull(boxed.getDoubles());
+        assertNull(boxed.getFloats());
+        assertNull(boxed.getInts());
+        assertNull(boxed.getLongs());
+        assertNull(boxed.getShorts());
+
+        boxed = new BoxedArrayClass();
+        primitive = ubiquity.map(boxed, PrimitiveArrayClass.class);
+
+        assertNull(primitive.getBools());
+        assertNull(primitive.getBytes());
+        assertNull(primitive.getChars());
+        assertNull(primitive.getDoubles());
+        assertNull(primitive.getFloats());
+        assertNull(primitive.getInts());
+        assertNull(primitive.getLongs());
+        assertNull(primitive.getShorts());
+    }
+
+    @Test
+    public void testPrimitiveToBoxed() {
+        PrimitiveArrayClass primitive = new PrimitiveArrayClass();
+        primitive.setBools(new boolean[] {true, false, false});
+        primitive.setBytes(new byte[] {0xc, 0xa, 0xf, 0xe, 0xb, 0xa, 0xb, 0xe});
+        primitive.setChars(new char[] {'c', 'a', 'f', 'e', ' ', 'b', 'a', 'b', 'e'});
+        primitive.setDoubles(new double[] {0d, 20d, 200d});
+        primitive.setFloats(new float[]{1f, 5f});
+        primitive.setInts(new int[]{0, 3, 6, 9});
+        primitive.setLongs(new long[]{5l, 250l});
+        primitive.setShorts(new short[]{9, 8, 7, 6});
+
+        BoxedArrayClass boxed = ubiquity.map(primitive, BoxedArrayClass.class);
+
+        assertNotNull(boxed.getBools());
+        assertNotNull(boxed.getBytes());
+        assertNotNull(boxed.getChars());
+        assertNotNull(boxed.getDoubles());
+        assertNotNull(boxed.getFloats());
+        assertNotNull(boxed.getInts());
+        assertNotNull(boxed.getLongs());
+        assertNotNull(boxed.getShorts());
+
+        assertEquals(primitive.getBools().length, boxed.getBools().length);
+        assertEquals(primitive.getBytes().length, boxed.getBytes().length);
+        assertEquals(primitive.getChars().length, boxed.getChars().length);
+        assertEquals(primitive.getDoubles().length, boxed.getDoubles().length);
+        assertEquals(primitive.getFloats().length, boxed.getFloats().length);
+        assertEquals(primitive.getInts().length, boxed.getInts().length);
+        assertEquals(primitive.getLongs().length, boxed.getLongs().length);
+        assertEquals(primitive.getShorts().length, boxed.getShorts().length);
+
+        for(int i = 0; i < primitive.getBools().length; i++) {
+            assertEquals((Object)primitive.getBools()[i], boxed.getBools()[i]);
+        }
+        for(int i = 0; i < primitive.getBytes().length; i++) {
+            assertEquals((Object)primitive.getBytes()[i], boxed.getBytes()[i]);
+        }
+        for(int i = 0; i < primitive.getChars().length; i++) {
+            assertEquals((Object)primitive.getChars()[i], boxed.getChars()[i]);
+        }
+        for(int i = 0; i < primitive.getDoubles().length; i++) {
+            assertEquals(primitive.getDoubles()[i], boxed.getDoubles()[i]);
+        }
+        for(int i = 0; i < primitive.getFloats().length; i++) {
+            assertEquals(primitive.getFloats()[i], boxed.getFloats()[i]);
+        }
+        for(int i = 0; i < primitive.getInts().length; i++) {
+            assertEquals((Object)primitive.getInts()[i], boxed.getInts()[i]);
+        }
+        for(int i = 0; i < primitive.getLongs().length; i++) {
+            assertEquals((Object)primitive.getLongs()[i], boxed.getLongs()[i]);
+        }
+        for(int i = 0; i < primitive.getShorts().length; i++) {
+            assertEquals((Object)primitive.getShorts()[i], boxed.getShorts()[i]);
+        }
+    }
+
+    @Test
+    public void testBoxedToPrimitive() {
+        BoxedArrayClass boxed = new BoxedArrayClass();
+        boxed.setBools(new Boolean[] {true, false, false});
+        boxed.setBytes(new Byte[] {0xc, 0xa, 0xf, 0xe, 0xb, 0xa, 0xb, 0xe});
+        boxed.setChars(new Character[] {'c', 'a', 'f', 'e', ' ', 'b', 'a', 'b', 'e'});
+        boxed.setDoubles(new Double[] {0d, 20d, 200d});
+        boxed.setFloats(new Float[]{1f, 5f});
+        boxed.setInts(new Integer[]{0, 3, 6, 9});
+        boxed.setLongs(new Long[]{5l, 250l});
+        boxed.setShorts(new Short[]{9, 8, 7, 6});
+
+        PrimitiveArrayClass primitive = ubiquity.map(boxed, PrimitiveArrayClass.class);
+
+        assertNotNull(primitive.getBools());
+        assertNotNull(primitive.getBytes());
+        assertNotNull(primitive.getChars());
+        assertNotNull(primitive.getDoubles());
+        assertNotNull(primitive.getFloats());
+        assertNotNull(primitive.getInts());
+        assertNotNull(primitive.getLongs());
+        assertNotNull(primitive.getShorts());
+
+        assertEquals(boxed.getBools().length, primitive.getBools().length);
+        assertEquals(boxed.getBytes().length, primitive.getBytes().length);
+        assertEquals(boxed.getChars().length, primitive.getChars().length);
+        assertEquals(boxed.getDoubles().length, primitive.getDoubles().length);
+        assertEquals(boxed.getFloats().length, primitive.getFloats().length);
+        assertEquals(boxed.getInts().length, primitive.getInts().length);
+        assertEquals(boxed.getLongs().length, primitive.getLongs().length);
+        assertEquals(boxed.getShorts().length, primitive.getShorts().length);
+
+        for(int i = 0; i < boxed.getBools().length; i++) {
+            assertEquals((Object)boxed.getBools()[i], primitive.getBools()[i]);
+        }
+        for(int i = 0; i < boxed.getBytes().length; i++) {
+            assertEquals((Object)boxed.getBytes()[i], primitive.getBytes()[i]);
+        }
+        for(int i = 0; i < boxed.getChars().length; i++) {
+            assertEquals((Object)boxed.getChars()[i], primitive.getChars()[i]);
+        }
+        for(int i = 0; i < boxed.getDoubles().length; i++) {
+            assertEquals(boxed.getDoubles()[i], primitive.getDoubles()[i]);
+        }
+        for(int i = 0; i < boxed.getFloats().length; i++) {
+            assertEquals(boxed.getFloats()[i], primitive.getFloats()[i]);
+        }
+        for(int i = 0; i < boxed.getInts().length; i++) {
+            assertEquals((Object)boxed.getInts()[i], primitive.getInts()[i]);
+        }
+        for(int i = 0; i < boxed.getLongs().length; i++) {
+            assertEquals((Object)boxed.getLongs()[i], primitive.getLongs()[i]);
+        }
+        for(int i = 0; i < boxed.getShorts().length; i++) {
+            assertEquals((Object)boxed.getShorts()[i], primitive.getShorts()[i]);
         }
     }
 }
