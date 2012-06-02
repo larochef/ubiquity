9dcd7769eb9333e8e00941f2f3d6d125982e46d0
Francois LAROCHE <flaroche@gmail.com>
2012-06-02 02:12:21 +0200

    Move annotations tests to their own file

--- src/test/java/org/ubiquity/bytecode/PropertyRetrieverVisitorTest.java
+++ src/test/java/org/ubiquity/bytecode/PropertyRetrieverVisitorTest.java
@@ -95,38 +95,4 @@ public class PropertyRetrieverVisitorTest {
         assertEquals("Parent property 1", result.getParent().getProperty1());
     }
 
-    @Test
-    public void testAnnotationsParsing() throws IOException{
-        ClassReader reader = new ClassReader("org/ubiquity/bytecode/AnnotatedClass");
-        PropertyRetrieverVisitor visitor = new PropertyRetrieverVisitor();
-        reader.accept(visitor, 0);
-        Map<String, Property> properties = visitor.getProperties();
-        assertFalse(properties.isEmpty());
-        assertTrue(properties.containsKey("property1"));
-        Property property1 = properties.get("property1");
-        assertFalse(property1.getAnnotations().isEmpty());
-        assertTrue(property1.getAnnotations().contains("Lorg/ubiquity/annotation/CopyRename;:property3:Lorg/ubiquity/bytecode/SimpleTestClass;"));
-
-        Property property3 = properties.get("property3");
-        assertNotNull(property3);
-        assertFalse(property3.getAnnotations().isEmpty());
-        assertTrue(property3.getAnnotations().contains("Lorg/ubiquity/annotation/CopyRename;:test:*"));
-        assertTrue(property3.getAnnotations().contains("Lorg/ubiquity/annotation/CopyRename;:property3:Lorg/ubiquity/bytecode/SimpleTestClass;"));
-    }
-
-    @Test
-    public void testSimpleRenaming() throws Exception {
-        AnnotatedClass2 object = new AnnotatedClass2();
-        object.setToto("toto");
-
-
-        CopyContext ctx = new CopyContext();
-        Copier<AnnotatedClass2, SimpleTestClass> copier =
-                new CopierGenerator().createCopier(AnnotatedClass2.class, SimpleTestClass.class, ctx);
-
-        SimpleTestClass dest = copier.map(object);
-        assertNotNull(dest);
-        assertEquals("toto", dest.getProperty1());
-    }
-
 }
