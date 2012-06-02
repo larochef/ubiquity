a3308937a94b5246a9bd6c839065b893779e17c2
Francois LAROCHE <flaroche@gmail.com>
2012-06-02 02:01:02 +0200

    Imports

--- src/test/java/org/ubiquity/bytecode/PropertyRetrieverVisitorTest.java
+++ src/test/java/org/ubiquity/bytecode/PropertyRetrieverVisitorTest.java
@@ -9,8 +9,6 @@ import org.objectweb.asm.ClassReader;
 import org.ubiquity.Copier;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static junit.framework.Assert.*;
