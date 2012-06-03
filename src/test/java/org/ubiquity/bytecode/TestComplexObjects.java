60a3c0aa3194b1d5f552c41c1eceea9059eb927c
Francois LAROCHE <flaroche@gmail.com>
2012-06-03 08:31:37 +0200

    Change test to test multiple copiers too

--- src/test/java/org/ubiquity/bytecode/TestComplexObjects.java
+++ src/test/java/org/ubiquity/bytecode/TestComplexObjects.java
@@ -12,18 +12,6 @@ public class TestComplexObjects {
     private static final Ubiquity ubiquity = new Ubiquity();
 
     public static class ComplexClass1 {
-        private ComplexClass1 parent;
-
-        public ComplexClass1 getParent() {
-            return parent;
-        }
-
-        public void setParent(ComplexClass1 parent) {
-            this.parent = parent;
-        }
-    }
-
-    public static class ComplexClass2 {
         private ComplexClass2 parent;
 
         public ComplexClass2 getParent() {
@@ -35,13 +23,25 @@ public class TestComplexObjects {
         }
     }
 
+    public static class ComplexClass2 {
+        private ComplexClass1 parent;
+
+        public ComplexClass1 getParent() {
+            return parent;
+        }
+
+        public void setParent(ComplexClass1 parent) {
+            this.parent = parent;
+        }
+    }
+
     @Test
     public void testComplexCopies() {
         ComplexClass1 src = new ComplexClass1();
         ComplexClass2 dest = ubiquity.map(src, ComplexClass2.class);
         assertNotNull(dest);
         assertNull(dest.getParent());
-        src.setParent(new ComplexClass1());
+        src.setParent(new ComplexClass2());
         dest = ubiquity.map(src, ComplexClass2.class);
         assertNotNull(dest);
         assertNotNull(dest.getParent());
