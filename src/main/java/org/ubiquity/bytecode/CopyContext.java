ec87f834c397ea4f42a56bb4a140defc1952a883
Francois LAROCHE <flaroche@gmail.com>
2012-06-07 21:12:56 +0200

    Use Guava's Loading cache to avoid synchronization problems and simplify all this part

--- src/main/java/org/ubiquity/bytecode/CopyContext.java
+++ src/main/java/org/ubiquity/bytecode/CopyContext.java
@@ -1,5 +1,8 @@
 package org.ubiquity.bytecode;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.ubiquity.CollectionFactory;
 import org.ubiquity.Copier;
 import org.ubiquity.logging.Logger;
@@ -7,11 +10,7 @@ import org.ubiquity.logging.impl.JdkLogging;
 import org.ubiquity.util.CopierKey;
 import org.ubiquity.util.DefaultCollectionFactory;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayDeque;
-import java.util.Map;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Class storing the different copiers, making all copiers available to other copiers.
@@ -24,77 +23,36 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public final class CopyContext {
 
-    private final Map<CopierKey<?,?>, Copier<?,?>> copiers;
-    private final Queue<CopierKey<?,?>> requiredTuples;
-    private final CopierGenerator generator;
+    private static final CopierGenerator generator = new CopierGenerator();
+
+    private final LoadingCache<CopierKey<?,?>, Copier<?,?>> cache = CacheBuilder.newBuilder()
+                                    .build(new CacheLoader<CopierKey<?,?>,Copier<?, ?>>(){
+                                        @Override
+                                        public Copier<?, ?> load(CopierKey<?, ?> copierKey) throws Exception {
+                                            return generator.createCopier(copierKey, CopyContext.this);
+                                        }
+                                    });
     private CollectionFactory factory;
     private final Logger logger;
 
     public CopyContext() {
-        this.copiers = new ConcurrentHashMap<CopierKey<?,?>, Copier<?, ?>>();
-        this.requiredTuples = new ArrayDeque<CopierKey<?, ?>>();
-        this.generator = new CopierGenerator();
         this.factory = new DefaultCollectionFactory();
         this.logger = JdkLogging.getJdkLoggerFactory().getLogger(CopyContext.class);
-
         this.registerCopier(CopierKey.newBuilder(Object.class, Object.class).build(), new DefaultCopier(this));
     }
 
-    public final <T, U> Copier<T,U> getCopier(CopierKey<T, U> key) {
-        @SuppressWarnings("unchecked") Copier<T, U> result = (Copier<T, U>) copiers.get(key);
-        if(result == null) {
-            result = this.createCopier(key);
-            if(result == null) {
-                throw new IllegalStateException("Unable to find the builder, it was supposed to be built.");
-            }
+    public final <T, U> Copier<T,U> getCopier(final CopierKey<T, U> key) {
+        try {
+            @SuppressWarnings("unchecked") Copier<T, U> copier = (Copier<T,U>) cache.get(key);
+            return copier;
+        } catch (ExecutionException e) {
+            logger.error("Unable to create copier : ", e);
         }
-        return result;
+        return getCopier(key);
     }
-
-
-    private <T, U> Copier<T,U> createCopier(CopierKey<T, U> key) {
-        // Group the 2 calls in a single synchronized block, to avoid 2 consecutive locks.
-        synchronized (this.requiredTuples) {
-            this.requireCopier(key);
-            try {
-                this.createRequiredCopiers();
-                @SuppressWarnings("unchecked") Copier<T, U> copier = (Copier<T, U>) copiers.get(key);
-                return copier;
-            } catch (InstantiationException e) {
-                logger.error("Unable to create copier : ", e);
-            } catch (IllegalAccessException e) {
-                logger.error("Unable to create copier : ", e);
-            } catch (InvocationTargetException e) {
-                logger.error("Unable to create copier : ", e);
-            } catch (NoSuchMethodException e) {
-                logger.error("Unable to create copier : ", e);
-            }
-        }
-        return null;
-    }
-
 
     public final <T, U> void registerCopier(CopierKey<T,U> key, Copier<T,U> copier) {
-        this.copiers.put(key, copier);
-    }
-
-    final void requireCopier(CopierKey<?,?> key) {
-        // Require copier so that the conversion can be done.
-        synchronized (this.requiredTuples) {
-            this.requiredTuples.add(key);
-        }
-    }
-
-    final <T, U> void createRequiredCopiers() throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
-        synchronized (this.requiredTuples) {
-            while(!this.requiredTuples.isEmpty()) {
-                @SuppressWarnings("unchecked") CopierKey<T, U> key = (CopierKey<T, U>) this.requiredTuples.remove();
-                // If copier already exists, nothing to do.
-                if(!copiers.containsKey(key)) {
-                    registerCopier(key, this.generator.createCopier(key, this));
-                }
-            }
-        }
+        this.cache.put(key, copier);
     }
 
     public final CollectionFactory getFactory() {
