1be114f640474eb04f4eb40c0a9dc0cf3723bdda
Francois LAROCHE <flaroche@gmail.com>
2013-05-05 22:06:36 +0200

    Change exception used to have one with a more appropriate name

--- mirror/src/main/java/org/ubiquity/mirror/impl/AbstractMirror.java
+++ mirror/src/main/java/org/ubiquity/mirror/impl/AbstractMirror.java
@@ -1,6 +1,5 @@
 package org.ubiquity.mirror.impl;
 
-import com.google.common.collect.ImmutableMap;
 import org.ubiquity.mirror.Function;
 import org.ubiquity.mirror.Mirror;
 import org.ubiquity.mirror.Property;
@@ -15,13 +14,14 @@ public abstract class AbstractMirror<T> implements Mirror<T> {
     private final Map<String, Property<T, ?>> properties = buildProperties();
 
     @Override
+    @SuppressWarnings("unchecked") // syntaxic sugar, callers should know what to expect.
     public <U> Property<T, U> getProperty(String name) {
         return (Property<T, U>) properties.get(name);
     }
 
     @Override
     public <U> Function<T, U> getFunction(String name) {
-        throw new IllegalStateException("Not implemented yet");
+        throw new UnsupportedOperationException("Not implemented yet");
     }
 
     protected abstract Map<String, Property<T, ?>> buildProperties();
