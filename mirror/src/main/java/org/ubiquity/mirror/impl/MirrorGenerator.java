2eaf5e6dd48824f34f13880d07c5260fb6a6af1d
Francois LAROCHE <laroche.f@sfeir.com>
2014-03-31 15:00:01 +0200

    Handle annotation properties inside an annotation

--- mirror/src/main/java/org/ubiquity/mirror/impl/MirrorGenerator.java
+++ mirror/src/main/java/org/ubiquity/mirror/impl/MirrorGenerator.java
@@ -184,7 +184,7 @@ public final class MirrorGenerator {
 
         for(Map.Entry<String, AnnotationProperty> prop : annotation.getProperties().entrySet()) {
             visitor.visitLdcInsn(prop.getKey());
-            mapAnnotationProperty(visitor, prop.getValue());
+            mapAnnotationProperties(visitor, prop.getValue());
             visitor.visitMethodInsn(INVOKEVIRTUAL, "com/google/common/collect/ImmutableMap$Builder", "put",
                     "(Ljava/lang/Object;Ljava/lang/Object;)Lcom/google/common/collect/ImmutableMap$Builder;");
         }
@@ -195,52 +195,62 @@ public final class MirrorGenerator {
                 "(Ljava/lang/Class;ZLjava/util/Map;)V");
     }
 
-    private static void mapAnnotationProperty(MethodVisitor visitor, AnnotationProperty property) {
+    private static void mapAnnotationProperties(MethodVisitor visitor, AnnotationProperty property) {
         visitor.visitTypeInsn(NEW, "org/ubiquity/mirror/AnnotationProperty");
         visitor.visitInsn(DUP);
         visitor.visitLdcInsn(property.getName());
         visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
-        mapAnnotationValue(visitor, property);
+        mapAnnotationClassAndValue(visitor, property);
         visitor.visitMethodInsn(INVOKESPECIAL, "org/ubiquity/mirror/AnnotationProperty", "<init>",
                 "(Ljava/lang/String;Ljava/lang/Class;Ljava/lang/Object;)V");
     }
 
-    private static void mapAnnotationValue(MethodVisitor visitor, AnnotationProperty property) {
+    private static void mapAnnotationClassAndValue(MethodVisitor visitor, AnnotationProperty property) {
         Class valueClass = toJavaClass(byteCodeName(property.getDesc()));
 
         if (valueClass == Integer.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Integer", "valueOf", "(I)Ljava/lang/Integer;");
         } else if (valueClass == Short.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Short", "valueOf", "(S)Ljava/lang/Short;");
         } else if (valueClass == Long.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Long", "valueOf", "(J)Ljava/lang/Long;");
         } else if (valueClass == Float.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Float", "valueOf", "(F)Ljava/lang/Float;");
         } else if (valueClass == Double.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Double", "valueOf", "(D)Ljava/lang/Double;");
         } else if (valueClass == Byte.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Byte", "valueOf", "(B)Ljava/lang/Byte;");
         } else if (valueClass == Boolean.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Boolean", "valueOf", "(Z)Ljava/lang/Boolean;");
         } else if (valueClass == Character.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
             visitor.visitMethodInsn(INVOKESTATIC, "java/lang/Character", "valueOf", "(C)Ljava/lang/Character;");
         } else if (valueClass == String.class) {
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName(property.getDesc()))));
             visitor.visitLdcInsn(property.getValue());
         } else if(valueClass.isEnum()) {
             final String byteCodeName = byteCodeName(valueClass.getName());
+            visitor.visitLdcInsn(Type.getType(toJavaClass(byteCodeName)));
             visitor.visitFieldInsn(GETSTATIC, byteCodeName, property.getValue().toString(), getDescription(byteCodeName));
         } else if(valueClass.isAnnotation()) {
-            // TODO : implement.me
+            visitor.visitLdcInsn(Type.getType(org.ubiquity.mirror.Annotation.class));
+            mapAnnotation(visitor, (Annotation) property.getValue());
         } else {
-
             throw new IllegalArgumentException("Unable to map (yet) class of type " + valueClass.getName());
         }
     }
--- mirror/src/test/java/org/ubiquity/mirror/AnnotationTest.java
+++ mirror/src/test/java/org/ubiquity/mirror/AnnotationTest.java
@@ -1,16 +1,25 @@
+/*
+ * Copyright 2012 ubiquity-copy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ubiquity.mirror;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.util.CheckClassAdapter;
-import org.objectweb.asm.util.TraceClassVisitor;
-import org.ubiquity.mirror.impl.MirrorGenerator;
 import org.ubiquity.mirror.objects.*;
 import org.ubiquity.mirror.util.Mirrors;
-import org.ubiquity.util.NativeConverter;
 
-import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 
@@ -64,5 +73,16 @@ public class AnnotationTest {
         Assert.assertTrue(propertyMap.containsKey("value"));
         final AnnotationProperty value = propertyMap.get("value");
         Assert.assertSame(TestEnum.VALUE1, value.getValue());
+    }
+
+    @Test
+    public void testAnnotatedAnnotations() {
+        Mirror<AnnotatedAnnotationObject> mirror = Mirrors.newMirrorFactory().getMirror(AnnotatedAnnotationObject.class);
+        final List<Annotation> annotations = mirror.getProperty("property").getAnnotations();
+        Assert.assertEquals(1, annotations.size());
+        final Annotation annotation1 = annotations.iterator().next();
+        final AnnotationProperty annotationProperty = annotation1.getProperties().get("value");
+        final Annotation annotation2 = (Annotation) annotationProperty.getValue();
+        Assert.assertEquals("Annotation !", annotation2.getProperties().get("value").getValue());
     }
 }
